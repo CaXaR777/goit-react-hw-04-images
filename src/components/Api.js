080952b99a6198bc45c5ed54ee3a8725cbe26fac
caxar777 <caxar7dam@gmail.com>
2023-06-19 12:26:22 +0200

    1

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,17 +1,17 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const searchParams = new URLSearchParams({
-//   key: '36043083-bbc675b79bda412dd952998c9',
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: 'true',
-//   per_page: 12,
-//   min_height: '180',
-// });
+const searchParams = new URLSearchParams({
+  key: '36043083-bbc675b79bda412dd952998c9',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: 12,
+  min_height: '180',
+});
 
-// export async function apiFetch(searchQuery, page) {
-//   const response = await axios.get(
-//     https://pixabay.com/api/?${searchParams}&page=${page}&q=${searchQuery}
-//   );
-//   return response;
-// }
+export async function apiFetch(searchQuery, page) {
+  const response = await axios.get(
+    `https://pixabay.com/api/?${searchParams}&page=${page}&q=${searchQuery}`
+    );
+  return response;
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+// import { Searchbar } from "./Searchbar/Searchbar";
+// import { ImageGallery } from "./ImageGallery/ImageGallery";
+// import { apiFetch } from "./Api/Api";
+// import { Button } from "./Button/Button";
+// import { Loader } from "./Loader/Loader";
+// import { Message } from "./Message/Message";
+
+export class App extends Component  {
+  state = {
+    searchPromt: '',
+    page: 1,
+    imagesArr: [],
+    totalImages: 0,
+    status: 'idle'
+  }
+
+
+
+  render() {
+    const {status, imagesArr, totalImages} = this.state
+   return (
+     <>
+       <Searchbar onSubmit={this.submitHandler} />
+       <div className="container">
+         {status === 'idle' ? <Message text='Fulfill your imagination'/>
+           : status === 'pending' ? <Loader />
+           : status === 'resolved' ? imagesArr.length === 0 ? <Message text='Nobody here but us chickens!'/> : <><ImageGallery data={imagesArr} />{totalImages > imagesArr.length ? <Button loadMore={this.loadMoreImg} /> : <Message text='Hope it`s fulfilled'/>}</>
+           : <Message text='Something went wrong'/>
+       </div>
+     </>);
+ } 
 };
