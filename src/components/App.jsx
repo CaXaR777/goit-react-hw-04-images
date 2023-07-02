1bc4463225dcecfbba7d88bd63f1c9bcc7884584
caxar777 <caxar7dam@gmail.com>
2023-07-03 00:58:05 +0200

    12333

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,61 +41,85 @@
 //       </>
 //     );
 //   }
-import React, { Component } from "react";
+import  React, { useState,  } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
-import { ImageGallery } from "./ImageGallery/ImageGallery";
+import  ImageGallery  from "./ImageGallery/ImageGallery";
 import { apiFetch } from "./Api";
 import { Button } from "./Button/Button";
 import { Loader } from "./Loader/Loader";
 import { Message } from "./Message/Message";
 
-export class App extends Component {
-  state = {
-    searchPromt: '',
-    page: 1,
-    imagesArr: [],
-    totalImages: 0,
-    status: 'idle'
-  }
+export const App = () => {
+  // state = {
+  //   searchPromt: '',
+  //   page: 1,
+  //   imagesArr: [],
+  //   totalImages: 0,
+  //   status: 'idle'
+  // }
+  const [searchPromt, setSearchPromt] = useState('');
+  const [page, setPage] = useState(1);
+  const [imagesArr, setImagesArr] = useState([]);
+  const [totalImages, setTotalImages] = useState(0);
+  const [status, setStatus] = useState('idle');
 
-  submitHandler = (e) => {
-    // Обработка отправки формы поиска
-    // Выполнение запроса к API с использованием `searchTerm`
-    // Обновление состояния компонента с полученными результатами
-    e.preventDefault()
-    this.setState({ searchPromt: e.target.elements.search.value.trim(), page: 1 }, async () => {
-      if (!this.state.searchPromt) {
-      return
-      }
-      try {
-        this.setState({status: 'pending'})
-        const images = await apiFetch(this.state.searchPromt, this.state.page)
-        this.setState({ imagesArr: images.data.hits, totalImages: images.data.totalHits, status: 'resolved' })
-      } catch (e) {
-        this.setState({status: 'rejected'})
-      }
-    })
+  const submitHandler = async (e) => {
+    // e.preventDefault()
+    // this.setState({ searchPromt: e.target.elements.search.value.trim(), page: 1 }, async () => {
+    //   if (!this.state.searchPromt) {
+    //   return
+    //   }
+    //   try {
+    //     this.setState({status: 'pending'})
+    //     const images = await apiFetch(this.state.searchPromt, this.state.page)
+    //     this.setState({ imagesArr: images.data.hits, totalImages: images.data.totalHits, status: 'resolved' })
+    //   } catch (e) {
+    //     this.setState({status: 'rejected'})
+    //   }
+    // })
+
+    e.preventDefault();
+    const searchTerm = e.target.elements.search.value.trim();
+    setSearchPromt(searchTerm);
+    setPage(1);
+
+    if (!searchTerm) return;
+
+    try {
+      setStatus('pending');
+      const images = await apiFetch(searchTerm, 1);
+      setImagesArr(images.data.hits);
+      setTotalImages(images.data.totalHits);
+      setStatus('resolved');
+    } catch (error) {
+      setStatus('rejected');
+    }
     
   };
 
-  loadMoreImages = () => {
-    // Загрузка дополнительных изображений
-    // Обновление состояния компонента с новыми изображениями
-    this.setState({ page: this.state.page + 1 }, async () => {
-      try {
-     const images = await apiFetch(this.state.searchPromt, this.state.page)
-     this.setState(prevState => ({
-       imagesArr: [...prevState.imagesArr, ...images.data.hits]
-       }));
-     } catch (e) {
-       alert('Something went wrong')
-     }
-   })
-  
+  const loadMoreImages = async () => {
+  //   this.setState({ page: this.state.page + 1 }, async () => {
+  //     try {
+  //    const images = await apiFetch(this.state.searchPromt, this.state.page)
+  //    this.setState(prevState => ({
+  //      imagesArr: [...prevState.imagesArr, ...images.data.hits]
+  //      }));
+  //    } catch (e) {
+  //      alert('Something went wrong')
+  //    }
+  //  })
+  try {
+    const nextPage = page + 1;
+    const images = await apiFetch(searchPromt, nextPage);
+    setImagesArr((prevImagesArr) => [...prevImagesArr, ...images.data.hits]);
+    setPage(nextPage);
+  } catch (error) {
+    alert('Something went wrong');
+  }
   };
 
-  render() {
-    const { status, imagesArr, totalImages } = this.state;
+  
+    // const { status, imagesArr, totalImages } = this.state;
 
     const components = {
       idle: <Message text="Let's find what you need" />,
@@ -106,7 +130,7 @@ export class App extends Component {
         <>
           <ImageGallery data={imagesArr} />
           {totalImages > imagesArr.length ? (
-            <Button loadMore={this.loadMoreImages} />
+            <Button loadMore={loadMoreImages} />
           ) : (
             <Message text="Hope you found what you need" />
           )}
@@ -119,9 +143,9 @@ export class App extends Component {
 
     return (
       <React.Fragment>
-        <Searchbar onSubmit={this.submitHandler} />
+        <Searchbar onSubmit={submitHandler} />
         <div className="container">{componentToRender}</div>
       </React.Fragment>
     );
   }
-}
+
